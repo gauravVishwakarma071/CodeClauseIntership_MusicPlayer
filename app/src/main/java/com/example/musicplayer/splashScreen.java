f45a6b47473ae3d6e399f6849ae4a338e308050b
Gaurav Vishwakarma <gauravvishwakamra071@gmail.com>
2023-08-23 16:14:09 +0530

    23/8 first commit

---
+++ app/src/main/java/com/example/musicplayer/splashScreen.java
@@ -0,0 +1,30 @@
+package com.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                    startActivity(new Intent(splashScreen.this,MainActivity.class));
+
+                finish();
+            }
+        },3000);
+
+    }
+}
